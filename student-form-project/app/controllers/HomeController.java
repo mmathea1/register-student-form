7973c9782f650905877fa7a9a3b8098023f319d7
MishyMinty <mustbemishtaken@gmail.com>
2017-09-04 16:56:04 +0300

    done with controller and routes, fixing the fieldset error though

--- student-form-project/app/controllers/HomeController.java
+++ student-form-project/app/controllers/HomeController.java
@@ -1,6 +1,14 @@
 package controllers;
 
+import models.GradeLevel;
+import models.GradePointAverage;
+import models.Hobby;
+import models.Major;
+import models.Student;
+import play.data.Form;
 import play.mvc.*;
+import views.formdata.StudentFormData;
+import views.html.index;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -14,8 +22,38 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
-    public Result index() {
-        return ok(views.html.index.render());
+    public Result getIndex(long id) {
+        StudentFormData studentData = (id == 0) ? new StudentFormData() : models.Student.makeStudentFormData(id);
+        Form<StudentFormData> formData = Form.form(StudentFormData.class).fill(studentData);
+        return ok(views.html.index.render(
+        		formData,
+        		Hobby.makeHobbyMap(studentData),
+        		GradeLevel.getNameList(),
+        		GradePointAverage.makeGPAMap(studentData),
+        		Major.makeMajorMap(studentData)));
+    }
+    public Result postIndex(){
+    	Form<StudentFormData> formData = Form.form(StudentFormData.class).bindFromRequest();
+    	
+    	if(formData.hasErrors()){
+    		//don't call formData.get(), pass null to helpers instead
+    		flash("error", "Please correct errors above.");
+    		return badRequest(views.html.index.render(formData,
+    				Hobby.makeHobbyMap(null),
+    				GradeLevel.getNameList(),
+    				GradePointAverage.makeGPAMap(null),
+    				Major.makeMajorMap(null)));	
+    	}
+    	
+    	else{
+    		Student student = Student.makeInstance(formData.get());
+    		flash("success", "Student instance created/edited: " + student);
+    		return ok(views.html.index.render(formData,
+    				Hobby.makeHobbyMap(formData.get()),
+    				GradeLevel.getNameList(),
+    				GradePointAverage.makeGPAMap(formData.get()),
+    				Major.makeMajorMap(formData.get())));
+    	}
     }
 
 }
